10b28e2f445d2c5caa1e9ce599a74a847e023187
max7703 <max7703@live.fr>
2017-12-17 03:42:49 -0500

    modele equipe

---
+++ PokemonUnitedTE/PokemonUnitedTE/www/js/Equipe.js
@@ -0,0 +1,9 @@
+﻿var Equipe = function (id, nom, pokemon1, pokemon2, pokemon3, categorie, description) {
+    this.id = id;
+    this.nom = nom;
+    this.pokemon1 = pokemon1;
+    this.pokemon2 = pokemon2;
+    this.pokemon3 = pokemon3;
+    this.categorie = categorie;
+    this.description = description;
+}
